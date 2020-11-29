7c7333ac5a2bc0937844a0210e03e2187fce1f02
Mihai Anton <antonmihai58@gmail.com>
2020-11-29 19:01:45 +0100

    Adds localhost as allowed host

--- djangodockerblueprint/settings.py
+++ djangodockerblueprint/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "TRUE") == "TRUE"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*", "127.0.0.1"]
 
 
 # Application definition
